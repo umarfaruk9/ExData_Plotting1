67d72ba229d22e8969396df7e1245a1ea1ba1d49
umarfaruk9 <ukarimapord@mail.com>
2020-08-22 06:03:16 +0000

    plot2 code

--- plot2.R
+++ plot2.R
@@ -15,7 +15,7 @@ electricDT[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global
 electricDT[, dateTime := as.POSIXct(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S")]
 
 # Filter Dates for 2007-02-01 and 2007-02-02
-electricDT <- powerDT[(dateTime >= "2007-02-01") & (dateTime < "2007-02-03")]
+electricDT <- electricDT[(dateTime >= "2007-02-01") & (dateTime < "2007-02-03")]
 
 png("plot2.png", width=480, height=480)
 
@@ -25,3 +25,4 @@ plot(x = electricDT[, dateTime]
      , type="l", xlab="", ylab="Global Active Power (kilowatts)")
 
 dev.off()
+quartz()
